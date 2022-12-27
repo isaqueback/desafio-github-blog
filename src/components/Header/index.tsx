11adc8520f471ff760cf25025a08060537e52eea
Isaque Delfino <isa.quecosta00@gmail.com>
2022-12-27 00:25:31 +0000

    completed Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HeaderContainer, HeaderContent } from "./styles";
+import logo from "../../assets/logo.svg";
+import headerLeftEffect from "../../assets/header-left-effect.svg";
+import headerRightEffect from "../../assets/header-right-effect.svg";
+
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={headerLeftEffect} alt="imagem esquerda de decoração do cabeçalho" />
+                <img src={logo} alt="logo" />
+                <img src={headerRightEffect} alt="imagem direita de decoração do cabeçalho" />
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutContainer = styled.main`
+    /* margin:  -5.5rem 18rem; */
+
+`
